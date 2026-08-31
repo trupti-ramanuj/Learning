426431b2db5bf3ab153fab28b215a78350d519f6
TruptiRamanuj <ramanujtrupti2005@gmail.com>
2026-08-31 17:03:15 +0530

    add

--- js/Js_learning/11_fun_with_JS/test.js
+++ js/Js_learning/11_fun_with_JS/test.js
@@ -1,2 +1,25 @@
-const myArr = []
-    % DebugPrint(myArr)
+// const myArr = []
+//     % DebugPrint(myArr)
+
+// continious, Holey
+
+//SMI(small integer)
+//Packed element
+//Double(float, sting, function)
+
+
+const arrTwo = [1, 2, 3, 4, 5]
+//PACKED_SMI_ELEMENTS
+
+arrTwo.push(6.0)
+//PACKED_DOUBLE_ELEMENTS
+
+arrTwo.push('7')
+//PACKED_SMI_ELEMENTS
+
+arrTwo[10] = 11
+//HOLEY_ELEMENTS
+
+console.log(arrTwo);
+console.log(arrTwo.length);
+console.log(arrTwo[19]);
